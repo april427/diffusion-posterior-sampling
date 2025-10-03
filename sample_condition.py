7da3272d32e372fcd895f71cf9c4b1d5ee6cde9b
miranda-ulm <78689468+miranda-ulm@users.noreply.github.com>
2025-10-03 13:28:24 +0200

    change to 100 timesetp (faster)

--- sample_condition.py
+++ sample_condition.py
@@ -262,6 +262,7 @@ def main():
         'learning_rate',
         'num_epochs',
         'save_interval',
+        'epoch_save_interval',
         'log_interval',
         'dataset',
         'dataloader',
